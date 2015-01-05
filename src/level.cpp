f0395f689589431d15468d334633b52f2e420390
Mark Oates <euph_boy@hotmail.com>
2015-01-05 05:35:48 -0500

    Revert "Revert "created a new map for testing""
    This reverts commit fb23ee8b1a3bb1e18167cea595573155b6cfd2fd.

--- src/level.cpp
+++ src/level.cpp
@@ -14,21 +14,26 @@
 
 Level::Level()
 {
-	maps.push_back(sandbox_map());
-	maps.push_back(water_room());
-	maps.push_back(customize_character_room());
 	//maps.push_back(create_a_map_from_model_with_cherries_in_it("first_chaser", color::firebrick, "first_chaser_03.obj", "quarter.png", "first_chaser-_items_03.obj"));
 	//maps.push_back(create_a_map_from_model_with_cherries_in_it("Box Land", color::burlywood, "mpy_model_with_squares-02.obj", "try-dis.jpg", ""));
 	//maps.push_back(create_a_map_from_model_with_cherries_in_it("first_chaser", color::firebrick, "first_chaser-03.obj", "quarter.png", "first_chaser_items-03.obj"));
 	//maps.push_back(small_room_map());
 	//maps.push_back(climber_map());
 
-	maps.push_back(bouncy_trouncy());
 
+	maps.push_back(construct());
+
+
+
+	// these are the maps from the original slugs life
+	/*
+	maps.push_back(sandbox_map());
+	maps.push_back(water_room());
+	maps.push_back(customize_character_room());
+	maps.push_back(bouncy_trouncy());
 	maps.push_back(terrain_map(maps));
 	maps.push_back(first_world_hiding_under_a_leaf()); // this should be the last room in the release version
-
-
+	*/
 }
 
 void Level::list_all_entity2s()
